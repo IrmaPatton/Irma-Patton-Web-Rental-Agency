ae78f6a038df20d063cf206f9cf273ef829a05a1
Irma <ipatton@basecampcodingacademy.org>
2018-10-17 13:44:52 -0500

    Finished page data in javascript.js.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,23 @@
 const PAGE_DATA = {
-    pets: {
+    pet1: {
         name: "The Watcher",
         Price: "900.01",
-        LostFine: "1800.01",
-        info:
-            "Glowing green, 20 tentacles ,and we think shrap teeth? Lives in water. Even we don't know what it is."
+        LostFine: "1800",
+        info: "Glowing green, 20 tentacles ,and we think sharp teeth?\
+             Lives in water. Even we don't know what it is."
+    }
+    pet2: {
+        name: "Unicorn",
+        Price: "976.01",
+        LostFine: "1952",
+        info: "6 1/2 foot, 14-ton, hairy ,and ugly. Made from a skull 29,000 years old\
+        No magic, sorry."
+    }
+    pet3: {
+        name: "Griffon",
+        Price: "859.01",
+        LostFine: "1718",
+        info: "Known for guarding treasure and priceless possessions.\
+         A eagle and lion mix you don't want to miss."
     }
 };
