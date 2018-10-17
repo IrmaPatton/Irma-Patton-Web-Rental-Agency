660616387cd6708e32bb25188e8ac29d0340608b
Irma <ipatton@basecampcodingacademy.org>
2018-10-17 12:48:55 -0500

    started page date in javascript.js.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const PAGE_DATA = {
+    pets: {
+        name: "The Watcher",
+        Price: "900.01",
+        LostFine: "1800.01",
+        info:
+            "Glowing green, 20 tentacles ,and we think shrap teeth? Lives in water. Even we don't know what it is."
+    }
+};
