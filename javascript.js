d1458393a839a06cf4510aeecd16ac6e79713f5f
Irma <ipatton@basecampcodingacademy.org>
2018-10-22 09:16:54 -0500

    Added commas to page data in javascript.js.

--- javascript.js
+++ javascript.js
@@ -4,23 +4,26 @@ const PAGE_DATA = {
         name: "The Watcher",
         Price: "900.01",
         LostFine: "1800",
-        info: "Glowing green, 20 tentacles ,and we think sharp teeth?\
+        info:
+            "Glowing green, 20 tentacles ,and we think sharp teeth?\
              Lives in water. Even we don't know what it is."
-    }
+    },
     pet2: {
         pic: "images/unicorn.jpg",
         name: "Unicorn",
         Price: "976.01",
         LostFine: "1952",
-        info: "6 1/2 foot, 14-ton, hairy ,and ugly. Made from a skull 29,000 years old\
+        info:
+            "6 1/2 foot, 14-ton, hairy ,and ugly. Made from a skull 29,000 years old\
         No magic, sorry."
-    }
+    },
     pet3: {
         pic: "images/griffon.jpg",
         name: "Griffon",
         Price: "859.01",
         LostFine: "1718",
-        info: "Known for guarding treasure and priceless possessions.\
+        info:
+            "Known for guarding treasure and priceless possessions.\
          A eagle and lion mix you don't want to miss."
     }
 };
